80a126269febb2426b102a47df3c288a3c3a8037
Felipe <Felipe@192.168.1.3>
2017-04-10 22:11:42 -0300

    cccccc

---
+++ sysge/src/main/java/br/com/sysge/relatorios/to/PagamentoTO.java
@@ -0,0 +1,93 @@
+package br.com.sysge.relatorios.to;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class PagamentoTO implements Serializable{
+	
+	private static final long serialVersionUID = 8720356673083525518L;
+
+	private String formaPagamento;
+	
+	private String parcelamento;
+	
+	private String numero;
+	
+	private BigDecimal parcela = BigDecimal.ZERO;
+	
+	private BigDecimal desconto = BigDecimal.ZERO;;
+	
+	private BigDecimal valorCobrado = BigDecimal.ZERO;;
+	
+	private String atendente;
+	
+	private Date vencimento;
+
+	public String getFormaPagamento() {
+		return formaPagamento;
+	}
+
+	public void setFormaPagamento(String formaPagamento) {
+		this.formaPagamento = formaPagamento;
+	}
+
+	public String getParcelamento() {
+		return parcelamento;
+	}
+
+	public void setParcelamento(String parcelamento) {
+		this.parcelamento = parcelamento;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public BigDecimal getParcela() {
+		return parcela;
+	}
+
+	public void setParcela(BigDecimal parcela) {
+		this.parcela = parcela;
+	}
+
+	public BigDecimal getDesconto() {
+		return desconto;
+	}
+
+	public void setDesconto(BigDecimal desconto) {
+		this.desconto = desconto;
+	}
+
+	public BigDecimal getValorCobrado() {
+		return valorCobrado;
+	}
+
+	public void setValorCobrado(BigDecimal valorCobrado) {
+		this.valorCobrado = valorCobrado;
+	}
+
+	public Date getVencimento() {
+		return vencimento;
+	}
+
+	public void setVencimento(Date vencimento) {
+		this.vencimento = vencimento;
+	}
+
+	public String getAtendente() {
+		return atendente;
+	}
+
+	public void setAtendente(String atendente) {
+		this.atendente = atendente;
+	}
+	
+	
+
+}
